5765ac3ba922bc25df03a87773631fed31f3816a
Alicja Stelmach <alicjaastelmach@gmail.com>
2021-07-03 12:25:13 +0200

    added route to user profile

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Login from './views/Login';
 import Dashboard from './views/Dashboard';
+import Profile from './views/Profile/Profile';
 
 export default function App() {
   return (
@@ -11,9 +12,14 @@ export default function App() {
           <Route path="/dashboard">
             <Dashboard />
             <Link to="/">Link to Login(for testonly)</Link>
+            <Link to="/profile">Link to Profile(for testonly)</Link>
           </Route>
           <Route exact path="/">
             <Login />
+            <Link to="/dashboard">Link to Dashboard(for testonly)</Link>
+          </Route>
+          <Route path="/profile">
+            <Profile />
             <Link to="/dashboard">Link to Dashboard(for testonly)</Link>
           </Route>
         </Switch>
