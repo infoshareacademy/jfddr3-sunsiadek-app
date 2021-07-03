b6f3b85309b3db6586b41d1accc0abb22f366073
Alicja Stelmach <alicjaastelmach@gmail.com>
2021-07-03 13:00:56 +0200

    added ProfileNavBox component to Profile

--- src/views/Profile/Profile.js
+++ src/views/Profile/Profile.js
@@ -1,8 +1,41 @@
+import { Button , TextField } from '@material-ui/core';
+
+import MultilineTextFields from '../../components/TextArea';
+import styled from 'styled-components';
+import { NavigationBox } from '../../components/NavigationBox';
+import { ProfileNavBox } from '../../components/ProfileNaxBox';
+
+const StyledProfile = styled.div`
+  margin: 10px;
+`;
+
 function Profile() {
   return (
-    <div>
-      <p>PROFILE</p>
-    </div>
+    <>
+      <ProfileNavBox />
+
+      <StyledProfile>
+        <div className="Profile">
+          <form>
+            <TextField id="standard-basic" label="Name" />
+            <TextField id="standard-basic" label="Surname" />
+            <TextField id="standard-basic" label="Age" />
+            <TextField id="standard-basic" label="Phone number" />
+            <TextField id="standard-basic" label="City" />
+            <TextField
+              id="standard-multiline-static"
+              label="description"
+              multiline
+              rows={4}
+              defaultValue=""
+            />
+            <Button variant="outlined" color="primary">
+              Save
+            </Button>
+          </form>
+        </div>
+      </StyledProfile>
+    </>
   );
 }
 
