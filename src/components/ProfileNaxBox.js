0dbe3309ae885fff4b6251908fcdcd9a57517089
Alicja Stelmach <alicjaastelmach@gmail.com>
2021-07-03 12:57:15 +0200

    added navigation box component for profile

---
+++ src/components/ProfileNaxBox.js
@@ -0,0 +1,24 @@
+import CustomizedMenus from './Menu';
+import styled from 'styled-components';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+
+const NavBox = styled.div`
+  width: 90%;
+  /* background-color: red; */
+  margin: 0 auto;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  /* background-color: red; */
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const ProfileNavBox = () => {
+  return (
+    <NavBox>
+      <img src="./images/logo.png" width="45" alt="This is a logo" />
+      <ExitToAppIcon />
+    </NavBox>
+  );
+};
