2f145adabfe91500d18a002f53bfd2fa0c6e2536
Monika Teslak <monika.teslak@gmail.com>
2021-07-05 14:05:12 +0200

    announcements category part 1

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Typography, Link } from '@material-ui/core';
 import { Link as RouterLink } from 'react-router-dom';
-
+import { db } from '../firebase-config';
 const Category = ({ title }) => {
+  const [announcements, setAnnouncements] = useState([]);
+  useEffect(() => {
+    db.collection('announcements')
+      .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          setAnnouncements(doc.data());
+        });
+      })
+      .catch(error => {
+        console.log('Error getting documents: ', error);
+      });
+  }, []);
+
   return (
     <Container>
       <Typography as="h2">{title}</Typography>
