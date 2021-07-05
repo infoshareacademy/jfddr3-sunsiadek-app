2b943e512f51ed738902b74dce56c2becfe5e4fb
Jakub Molicki <molickikuba@gmail.com>
2021-07-05 11:51:31 +0200

    display one ad. We cannot yet state what ad we want to display

--- src/views/SingleAd/SingleAd.js
+++ src/views/SingleAd/SingleAd.js
@@ -1,16 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { db } from '../../firebase-config';
 
-export default function SingleAd(target) {
-  // db.collection('announcements')
-  //   .where('id', '==', target.id)
-  //   .onSnapshot(data => {
-  //     console.log(data.author.value);
-  //   });
+export default function SingleAd({ idA = 'lyntpy7s81lMGN1gmRKS' }) {
+  var docRef = db.collection('announcements').doc(idA);
 
+  const [announcement, setAnnouncement] = useState(null);
+
+  // // konwert daty na czytelny
+  // const myDateConvert = myDate.toDate();
+  // console.log(myDateConvert);
+
+  useEffect(() => {
+    docRef
+      .get()
+      .then(doc => {
+        if (doc.exists) {
+          console.log('Document data:', doc.data());
+          const currentAd = doc.data();
+          console.log('State');
+          console.log(announcement);
+          const dateConvert = announcement.dateCreated.toDate();
+          console.log(dateConvert);
+          setAnnouncement(currentAd);
+        } else {
+          console.log('No such document!');
+        }
+      })
+      .catch(error => {
+        console.log('Error getting document:', error);
+      });
+  }, []);
   return (
     <>
-      <div>UUuu</div>;<div>Uuuu</div>;
+      {announcement && (
+        <div>
+          <h2>{announcement.title}</h2>
+          <p>{announcement.descriptions}</p>
+          <p>{announcement.author}</p>
+          <p>{announcement.category}</p>
+          <p>{announcement.givetake}</p>
+          <p>{announcement.status}</p>
+          {/* <p>{announcement.myDateCOnvert}</p> */}
+        </div>
+      )}
     </>
   );
 }
