94d9ed7dae0ebc0fa0b9c5f048ec48d3516aaa11
molekJ <65181784+molekJ@users.noreply.github.com>
2021-07-07 19:53:45 +0200

    Update src/components/PrivateRoute.js
    Co-authored-by: Piotr Krasuski <krasuskip@gmail.com>

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -4,7 +4,6 @@ import { useAuth } from '../context/AuthProvider';
 
 export default function PrivateRoute({ component: Component, ...rest }) {
   const { currentUser } = useAuth();
-  console.log(currentUser);
   return (
     <Route
       {...rest}
