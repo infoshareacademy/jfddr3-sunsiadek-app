0382ebc90d35dbd891d99a0ef3cc6f661462be05
Piotr Krasuski <krasuskip@gmail.com>
2021-07-07 19:58:41 +0200

    Wyswietlenie ogloszeń w danej katerogii

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,27 +1,59 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Typography, Link } from '@material-ui/core';
+import {
+  Container,
+  Typography,
+  Link,
+  List,
+  ListItem,
+  ListItemText
+} from '@material-ui/core';
 import { Link as RouterLink } from 'react-router-dom';
 import { db } from '../firebase-config';
-const Category = ({ title }) => {
+
+const Category = ({ title: categoryTitle, categoryName }) => {
   const [announcements, setAnnouncements] = useState([]);
+
   useEffect(() => {
-    db.collection('announcements')
-      .then(querySnapshot => {
-        querySnapshot.forEach(doc => {
-          setAnnouncements(doc.data());
-        });
-      })
-      .catch(error => {
-        console.log('Error getting documents: ', error);
-      });
+    const fetchAnnouncements = async () => {
+      try {
+        const snapshot = await db
+          .collection('announcements')
+          .where('category', '==', categoryName)
+          .get();
+        setAnnouncements(snapshot.docs.map(doc => doc.data()));
+        console.log(snapshot);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+
+    fetchAnnouncements();
   }, []);
 
   return (
     <Container>
-      <Typography as="h2">{title}</Typography>
+      <Typography variant="h4">{categoryTitle}</Typography>
       <Link component={RouterLink} to="/dashboard">
-        Wróć
+        Wróć do listy kategorii
       </Link>
+
+      <List>
+        {announcements.map(({ title, descriptions, author }) => {
+          return (
+            <ListItem>
+              <ListItemText
+                primary={<Typography variant="h5">{title}</Typography>}
+                secondary={
+                  <>
+                    <Typography>Treść: {descriptions}</Typography>
+                    <Typography>Author: {author}</Typography>
+                  </>
+                }
+              ></ListItemText>
+            </ListItem>
+          );
+        })}
+      </List>
     </Container>
   );
 };
--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import ButtonBases from '../../components/DashboardMenuButtons';
 import { NavigationBox } from '../../components/NavigationBox';
 import { BrowserRouter as Switch, Route } from 'react-router-dom';
 import Category from '../../components/Category';
+import { CATEGORIES } from '../../constants';
 
 function Dashboard() {
   return (
@@ -10,22 +11,25 @@ function Dashboard() {
       <div className="Dashboard">
         <NavigationBox></NavigationBox>
         <Route path="/dashboard/other">
-          <Category title="Other" />
+          <Category title="Other" categoryName={CATEGORIES.OPTHER} />
         </Route>
         <Route path="/dashboard/events">
-          <Category title="events" />
+          <Category title="Events" categoryName={CATEGORIES.EVENTS} />
         </Route>
         <Route path="/dashboard/parking">
-          <Category title="parking" />
+          <Category
+            title="Parking Space"
+            categoryName={CATEGORIES.PARKING_SPACE}
+          />
         </Route>
         <Route path="/dashboard/helper">
-          <Category title="helper" />
+          <Category title="Helper" categoryName={CATEGORIES.HELPER} />
         </Route>
         <Route path="/dashboard/storage-space">
-          <Category title="storage-space" />
+          <Category title="Storage Space" categoryName={CATEGORIES.STORAGE} />
         </Route>
         <Route path="/dashboard/small-fixes">
-          <Category title="small-fixes" />
+          <Category title="Small Fixes" categoryName={CATEGORIES.SMALL_FIXES} />
         </Route>
         <Route exact path="/dashboard">
           <ButtonBases />
