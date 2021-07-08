4bb0c38b9ba2b092029ae7012199bc028bb093ea
Alicja Stelmach <alicjaastelmach@gmail.com>
2021-07-08 11:15:48 +0200

    add route to profile

--- src/app.js
+++ src/app.js
@@ -2,35 +2,23 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './views/Login';
 import Dashboard from './views/Dashboard';
-import Profile from './views/Profile/Profile';
 import Register from './views/Register';
 import { AuthProvider } from './context/AuthProvider';
 import PrivateRoute from './components/PrivateRoute';
 import AddingAds from './views/AddingAds/AddingAds';
+import Profile from './views/Profile/Profile';
 
 export default function App() {
   return (
     <Router>
       <AuthProvider>
-        <div>
-          <Switch>
-            <Route exact path="/login">
-              <Login />
-            </Route>
-            <PrivateRoute exact path="/dashboard">
-              <Dashboard />
-            </PrivateRoute>
-            <Route path="/register">
-              <Register />
-            </Route>
-            <PrivateRoute path="/post">
-              <AddingAds />
-            </PrivateRoute>
-            <PrivateRoute path="/profile">
-              <Profile />
-            </PrivateRoute>
-          </Switch>
-        </div>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <PrivateRoute exact path="/post" component={AddingAds} />
+          <PrivateRoute path="/profile" component={Profile} />
+          <PrivateRoute path="/" component={Dashboard} />
+        </Switch>
       </AuthProvider>
     </Router>
   );
