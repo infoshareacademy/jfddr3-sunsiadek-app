3b28b5b4d556d0e9bcab9680ddff5fb60539f56a
Alicja Stelmach <alicjaastelmach@gmail.com>
2021-07-10 21:08:12 +0200

    change style for Login & Register view

--- src/views/Register/Register.js
+++ src/views/Register/Register.js
@@ -14,17 +14,20 @@ import { useAuth } from '../../context/AuthProvider';
 
 import { Link, useHistory } from 'react-router-dom';
 
+const StyledApplication = styled.div`
+  background-color: #f2f3f4;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
 const StyledCard = styled(Card)`
   padding: 10px;
   height: auto;
-  width: 200px;
+  width: 300px;
   margin: 10px auto;
-`;
-
-const StyledButton = styled(Button)`
-  margin: auto;
-  background-color: gray;
-  width: auto;
+  border-radius: 5px;
 `;
 
 const StyledAvatar = styled(Avatar)`
@@ -56,51 +59,68 @@ export default function Register() {
   }
 
   return (
-    <Box align="center">
-      <StyledCard elevation={10}>
-        <Box align="center">
-          <StyledAvatar>
-            <LockOpenOutlinedIcon />
-          </StyledAvatar>
-          <Typography variant="h6"> Register</Typography>
-          {error && <p>{error}</p>}
-        </Box>
-        <form onSubmit={handleSubmit}>
-          <TextField
-            id="email"
-            type="email"
-            name="email"
-            label="E-mail"
-            required
-          />
-          <TextField
-            id="password"
-            name="password"
-            label="Password"
-            type="password"
-            required
-          />
-          <TextField
-            id="passwordconfirm"
-            name="passwordconfirm"
-            label="Password Confirmation"
-            type="password"
-          />
-          <Box>
-            <StyledButton
-              disabled={loading}
-              type="submit"
-              variant="contained"
-              color="primary"
-            >
-              Register
-            </StyledButton>
-            <Typography>
-              Masz juz konto? <Link to="/login">Sign In</Link>
-            </Typography>
+    <StyledApplication>
+      <Box align="center">
+        <StyledCard elevation={10}>
+          <Box align="center">
+            <StyledAvatar>
+              <LockOpenOutlinedIcon />
+            </StyledAvatar>
+            <Typography variant="h6">Register</Typography>
+            {error && <p>{error}</p>}
           </Box>
-        </form>
-      </StyledCard>
-    </Box>
+          <form onSubmit={handleSubmit}>
+            <TextField
+              id="email"
+              type="email"
+              name="email"
+              label="E-mail"
+              required
+            />
+            <TextField
+              id="password"
+              name="password"
+              label="Password"
+              type="password"
+              required
+            />
+            <TextField
+              id="passwordconfirm"
+              name="passwordconfirm"
+              label="Password Confirmation"
+              type="password"
+            />
+            <Box>
+              <Button
+                disabled={loading}
+                type="submit"
+                variant="contained"
+                color="primary"
+                style={{
+                  backgroundColor: '#2e4053',
+                  margin: '15px',
+                  borderRadius: '5px'
+                }}
+              >
+                Register
+              </Button>
+              <Typography style={{ fontWeight: '500', color: '#2e4053' }}>
+                Already have an account?{' '}
+                <Link
+                  style={{
+                    textDecoration: 'none',
+                    color: '#2e4053',
+                    fontWeight: '400'
+                  }}
+                  to="/login"
+                >
+                  click here to sign in 💻
+                </Link>
+              </Typography>
+            </Box>
+          </form>
+        </StyledCard>
+      </Box>
+    </StyledApplication>
   );
 }
