73237177f08a029a277a0b4e9fafff6d8c50afc1
Alicja Stelmach <alicjaastelmach@gmail.com>
2021-07-10 15:54:54 +0200

    add logout function to logout icon in profile

--- src/components/ProfileNaxBox.js
+++ src/components/ProfileNaxBox.js
@@ -1,6 +1,9 @@
 import CustomizedMenus from './Menu';
 import styled from 'styled-components';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import { useAuth } from '../context/AuthProvider';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 const NavBox = styled.div`
   width: 90%;
@@ -15,10 +18,27 @@ const NavBox = styled.div`
 `;
 
 export const ProfileNavBox = () => {
+  const [error, setError] = useState('');
+  const { logout } = useAuth();
+  const history = useHistory();
+
+  async function handleLogout() {
+    setError('');
+
+    try {
+      await logout();
+      history.push('/login');
+      window.location.reload();
+    } catch {
+      setError('Nie udało się wylogować');
+      console.lgo(error);
+    }
+  }
+
   return (
     <NavBox>
       <img src="./images/logo.png" width="45" alt="This is a logo" />
-      <ExitToAppIcon style={{ fontSize: 35 }} />
+      <ExitToAppIcon onClick={handleLogout} style={{ fontSize: 35 }} />
     </NavBox>
   );
 };
